45fa339fa5480d9475ddfcdea3d06d909c98f276
ssmr9dt@gmail.com <ssmr9dt@gmail.com>
2020-03-09 06:50:50 +0000

    guzzlehttp...

--- src/YammerMessage.class.php
+++ src/YammerMessage.class.php
@@ -13,6 +13,11 @@ class YammerMessage
 
 	public function __construct()
 	{
+		// for GuzzleHttp
+		// $this->request = new Client([
+		// 	"base_uri" => $this->url
+		// ]);
+		// end GuzzleHttp
 		$this->request = new HTTP_Request2($this->url, HTTP_Request2::METHOD_POST);
 		$this->request->setConfig([
 		    "ssl_verify_host" => false,
@@ -27,7 +32,14 @@ class YammerMessage
 		$arr["body"] = $body;
 		$arr["group_id"] = $group_id;
 		// $arr["cc"] = ["mailaddress","yammer ID"];
-
+		// $arr["direct_to_user_ids"] = [1111111111111];
+		
+		// for GuzzleHttp
+		// $this->request->post("", [
+		// 	"headers" => ["Authorization: Bearer " . YAMMER_TOKEN ],
+		// 	"json" => $arr
+		// ]);
+		// end GuzzleHttp
 		$this->request->setBody(json_encode($arr));
 		$ret = $this->request->send();
 		if ($ret->getStatus() <= 299) { return true; }
